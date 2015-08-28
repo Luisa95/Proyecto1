05b9d79143687cc27a09f53ebaa24ff89d5b990b
Luisa95 <luisaflopez33@gmail.com>
2015-08-27 20:24:17 -0500

    Se organizo la carpeta del proyecto al igual que algunos archivos

--- Proyecto1/registros.c
+++
@@ -1,6 +0,0 @@
-#include "registros.h"
-
-
-uint8_t Rd[16]=00000000;
-
-uint8_t Rr[16]=00000000;
--- Trabajo1/test/alu.h
+++ Trabajo1/test/alu.h
@@ -6,24 +6,35 @@ void ADD(int8_t Rd,int8_t Rr);
 /**
 *\brief funcion que suma
 *\param Rd operando 1
-*\param Rr operando 2 
-*\return un entero con el resultado
+*\param Rr operando 2
 **/
 void SUB(int8_t Rd,int8_t Rr);
 /**
 *\brief funcion que resta
 *\param Rd operando 1
-*\param Rr operando 2 
-*\return un entero con el resultado
+*\param Rr operando 2
 **/
 void AND(int8_t Rd,int8_t Rr);
 /**
-*\brief funcion que resta
+*\brief funcion de producto logico
 *\param Rd operando 1
-*\param Rr operando 2 
-*\return un entero con el resultado
+*\param Rr operando 2
 **/
 void OR(int8_t Rd,int8_t Rr);
+/**
+*\brief funcion de suma logica
+*\param Rd operando 1
+*\param Rr operando 2
+**/
 void EOR(int8_t Rd,int8_t Rr);
+/**
+*\brief funcion de or exclusiva
+*\param Rd operando 1
+*\param Rr operando 2
+**/
 void MOV(int8_t Rd, int8_t Rr);
-
+/**
+*\brief funcion que mueve un numero en otro
+*\param Rd operando 1
+*\param Rr operando 2
+**/
