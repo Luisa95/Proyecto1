375897e1c7f7322c48c6ec33916ba95af8f202bf
Luisa95 <luisaflopez33@gmail.com>
2015-08-28 14:42:28 -0500

    Se corrigieron varios errores en el codigo

--- Trabajo1/alu.c
+++ Trabajo1/alu.c
@@ -5,29 +5,34 @@ uint32_t ADD(uint32_t Rd,uint32_t Rr)
 {
     Rd=Rd+Rr;//suma 2 registros
     return Rd;
-};
+}
 
 uint32_t SUB(uint32_t Rd,uint32_t Rr)
 {
     Rd=Rd-Rr;//resta 2 registros
-};
+    return Rd;
+}
 
 uint32_t AND(uint32_t Rd,uint32_t Rr)
 {
     Rd=Rd&Rr;//and entre los registros
-};
+    return Rd;
+}
 
 uint32_t OR(uint32_t Rd,uint32_t Rr)
 {
     Rd=Rd|Rr;//or entre los registros
-};
+    return Rd;
+}
 
 uint32_t EOR(uint32_t Rd,uint32_t Rr)
 {
     Rd=Rd^Rr;//xor entre los registros
+    return Rd;
 };
 
 uint32_t MOV(uint32_t Rd,uint32_t Rr)
 {
     Rd=Rr;
+    return Rd;
 };
