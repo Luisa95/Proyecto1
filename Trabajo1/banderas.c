114064218d6101044573c48d94397b51ebc17522
JulioAtuesta <julioatuesta@gmail.com>
2015-08-28 22:47:27 -0500

    shadhhd

--- Trabajo1/banderas.c
+++ Trabajo1/banderas.c
@@ -1,1 +1,46 @@
+#include "banderas.h" 
 
+void banderas(uint32_t Rd,uint32_t Rn, uint32_t Rr,struct bandera *puntero)
+{
+    uint32_t numero = 4294967296; //2^32 bits
+
+    if(rx==0)
+    {
+        puntero->zero=1;
+    }
+    else
+    {
+        puntero->zero=0;
+    }
+    
+    if((1<<31)&rx)
+    {
+        puntero->negativo=1;
+    }
+    else
+    {
+        puntero->negativo=0;
+    }
+
+    
+
+
+    if(((Rn>numero)&&(Rr<numero)&&(Rd<numero))||((Rr>numero)&&(Rn<numero)&&(Rd<numero))||((Rn>numero)&&(Rr>numero)))
+    {
+        puntero->acarreo=1;
+    }
+    else
+    {
+        puntero->acarreo=0;
+    }
+
+    
+    if(((Rn>numero)&&(Rr>numero)&&(Rd<=numero))||((Rn<=numero)&&(Rr<=numero)&&(Rd>numero)))
+    {
+        puntero->sobreflujo=1;
+    }
+    else
+    {
+        puntero->sobreflujo=0;
+    }
+}
--- Trabajo1/banderas.h
+++ Trabajo1/banderas.h
@@ -1,3 +1,15 @@
-#include <stdio.h>
+#include<stdio>
+#include<stdint.h>
+#include<stdbool.h>
 
-void Negative()
+
+
+    struct bandera 
+    {
+        bool negativo;
+        bool zero;
+        bool acarreo;
+        bool sobreflujo;
+    };
+
+void banderas (uint32_t rx,uint32_t Rd, uint32_t Rr,struct bandera *puntero);
