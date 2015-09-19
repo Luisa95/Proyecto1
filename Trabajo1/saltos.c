3dc001e2a18a8c9f1f88c2ce7ebfaddbcc608224
Luisa95 <luisaflopez33@gmail.com>
2015-09-18 23:17:33 -0500

    Se organizaron algunas partes del codigo

--- Trabajo1/saltos.c
+++ Trabajo1/saltos.c
@@ -1,1 +1,66 @@
+#include "saltos.h"
 
+void B (pc, uint32_t Pos)
+{
+    pc=pc+2*Pos;
+}
+void BEQ()
+{
+
+}
+void BNE()
+{
+
+}
+void BCS()
+{
+
+}
+void BCC()
+{
+
+}
+void BMI()
+{
+
+}
+void BPL()
+{
+
+}
+void BVS()
+{
+
+}
+void BVC()
+{
+
+}
+void BHI()
+{
+
+}
+void BLS()
+{
+
+}
+void BGE()
+{
+
+}
+void BLT()
+{
+
+}
+void BGT()
+{
+
+}
+void BLE()
+{
+
+}
+void BAL()
+{
+
+}
--- Trabajo1/saltos.h
+++ Trabajo1/saltos.h
@@ -2,4 +2,20 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-//void BNE ()
+void B (pc, uint32_t);
+void BEQ();
+void BNE();
+void BCS();
+void BCC();
+void BMI();
+void BPL();
+void BVS();
+void BVC();
+void BHI();
+void BLS();
+void BGE();
+void BLT();
+void BGT();
+void BLE();
+void BAL();
+
